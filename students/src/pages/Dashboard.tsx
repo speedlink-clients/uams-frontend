97c41996294ecb30fbb2db1a63abb0856f314ac5
Lloyd-Emperor <honourakhigbe6@gmail.com>
2026-01-31 13:41:28 +0100

    git1.1

--- students/src/pages/Dashboard.tsx
+++ students/src/pages/Dashboard.tsx
@@ -64,7 +64,15 @@ const Dashboard: React.FC = () => {
     { label: 'Department', value: profile?.Department?.name  || 'Nil' },
     { label: 'Session', value: profile?.session?.name || 'Nil' },
     { label: 'Level', value: profile?.level ? `${profile.level} Level` : 'Nil' },
-    { label: 'Semester', value: profile?.activeSemester?.[0]?.name || 'Nil' },
+    { 
+      label: 'Semester', 
+      value: (() => {
+        const sem = profile?.activeSemester?.[0]?.name?.toLowerCase();
+        if (sem === 'semester 1') return 'First Semester';
+        if (sem === 'semester 2') return 'Second Semester';
+        return profile?.activeSemester?.[0]?.name || 'Nil';
+      })()
+    },
     { label: 'Matric No.', value: profile?.studentId || 'Nil' },
     { label: 'Program', value: profile?.program?.name || 'Nil' },
   ];
