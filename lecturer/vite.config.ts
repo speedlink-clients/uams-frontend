445f1c017e99aea59a256211653ff6fe1b73c397
Lloyd-Emperor <honourakhigbe6@gmail.com>
2026-02-23 09:28:41 +0100

    lecturer module initialization

--- lecturer/vite.config.ts
+++ lecturer/vite.config.ts
@@ -1,7 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/lecturer/',
+    server: {
+      allowedHosts: ['computerscience.uniport.edu.ng', 'localhost'],
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {},
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
 })
