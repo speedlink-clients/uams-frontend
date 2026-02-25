f403611ede34a91c1ebab5e05059374a09706186
Lloyd-Emperor <honourakhigbe6@gmail.com>
2026-02-25 10:19:45 +0100

    github 1.2

--- lecturer/src/components/ui/rich-text-editor-context.tsx
+++ lecturer/src/components/ui/rich-text-editor-context.tsx
@@ -1,23 +1,23 @@
-"use client"
+// "use client"
 
-import { Editor } from "@tiptap/react"
-import * as React from "react"
+// import { Editor } from "@tiptap/react"
+// import * as React from "react"
 
-export interface RichTextEditorContextValue {
-  editor: Editor | null
-}
+// export interface RichTextEditorContextValue {
+//   editor: Editor | null
+// }
 
-export const RichTextEditorContext =
-  React.createContext<RichTextEditorContextValue | null>(null)
+// export const RichTextEditorContext =
+//   React.createContext<RichTextEditorContextValue | null>(null)
 
-RichTextEditorContext.displayName = "RichTextEditorContext"
+// RichTextEditorContext.displayName = "RichTextEditorContext"
 
-export function useRichTextEditorContext() {
-  const context = React.useContext(RichTextEditorContext)
-  if (!context) {
-    throw new Error(
-      "useRichTextEditorContext must be used within a RichTextEditorRoot",
-    )
-  }
-  return context
-}
+// export function useRichTextEditorContext() {
+//   const context = React.useContext(RichTextEditorContext)
+//   if (!context) {
+//     throw new Error(
+//       "useRichTextEditorContext must be used within a RichTextEditorRoot",
+//     )
+//   }
+//   return context
+// }
