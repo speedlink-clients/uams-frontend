317d80bc44edf5c0751e0e0f17af2e226f9211e1
Lloyd-Emperor <honourakhigbe6@gmail.com>
2026-05-08 09:59:55 +0100

    department-admin auth fix

--- landing/src/app/auth/login/page.tsx
+++ landing/src/app/auth/login/page.tsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
                 window.location.href = "/student";
             } else if (userRoles.includes("STAFF")) {
                 if (userRoles.includes("DEPARTMENT_ADMIN")) {
-                    window.location.href = "/departmental";
+                    window.location.href = "/departmental-admin/";
                 } else {
                     window.location.href = "/lecturer";
                 }
