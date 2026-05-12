34d7826c5377afc520c603f4e17ebf4951a94d6f
Inameti <inameti.monday@speedlinkng.com>
2026-05-12 15:20:10 +0100

    refactor of the lecturer dashboard and courses page

--- lecturer/vite.config.ts
+++ lecturer/vite.config.ts
@@ -5,10 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
     base: '/lecturer/',
-    server: {
-      allowedHosts: ['computerscience.uniport.edu.ng', 'localhost', 'wrought-squeamish-mothball.ngrok-free.dev 80'],
-      port: 3000,
-      host: '0.0.0.0',
-    },
     plugins: [react(), tsconfigPaths()],
 })
